09a512368b0c2a96407f4c113010d83214e5fb14
Paulo Cruz <cruzh.paulo06@gmail.com>
2025-10-17 10:44:03 -0600

    viewport changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,13 +27,14 @@ export const metadata = {
     ],
     shortcut: '/favicon.ico',
     apple: '/favicon.ico',
-  },
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 5,
-  },
+  }
 }
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 5,
+};
 
 export default function RootLayout({
   children,
